7cd453b32b5c618f04206260b59e9d96ecf3c3a7
Christian <christian.sherland@dowjones.com>
2015-02-23 21:53:08 -0500

    linting this

--- services/lol-api/index.js
+++ services/lol-api/index.js
@@ -1,6 +1,6 @@
 var request = require('request'),
-  async = require('async'),
-  API_KEY = process.env.API_KEY;
+async = require('async'),
+API_KEY = process.env.API_KEY;
 
 module.exports = LOL_API = {};
 
@@ -8,24 +8,24 @@ module.exports = LOL_API = {};
 //  var requestUrl = 'https://${region}.api.pvp.net/api/lol/${region}/v1.4/summoner/by-name/${summoner_name}?api_key=${API_KEY}';
 LOL_API.getSummonerInfo = function(region, summoner_name, cb) {
   var requestUrl = 'https://'+
-    region +'.api.pvp.net/api/lol/'+
+  region +'.api.pvp.net/api/lol/'+
     region +'/v1.4/summoner/by-name/'+
     summoner_name +'?api_key='+
     API_KEY;
   request.get(requestUrl, function (err, response, body) {
     if (err) return cb(err);
     cb(null, JSON.parse(body));
-  })
-}
+  });
+};
 
 // var requestUrl = 'https://${region}.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/${platform_id}/${player_id}?api_key=${API_KEY}';
 
 LOL_API.getMatchInfo = function (region, player_id, cb) {
-  var region = 'na',
-   platform_id = 'NA1'; // TODO: MAP region to platformID
+  region = 'na';
+  var platform_id = 'NA1'; // TODO: MAP region to platformID
 
   var requestUrl = 'https://'+
-    region +'.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/'+
+  region +'.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/'+
     platform_id +'/'+ player_id +'?api_key='+
     API_KEY;
 
@@ -35,31 +35,26 @@ LOL_API.getMatchInfo = function (region, player_id, cb) {
       return cb(null, {
         status : 'fail',
         message : 'Player is not in a game'
-      })
+      });
     }
     cb(null, JSON.parse(body));
-  })
-}
+  });
+};
 
 
 LOL_API.getSpectateInfo = function(region, name, cb) {
   var self = this;
   async.waterfall([
     function (callback) {
-      self.getSummonerInfo(region, name, callback);
-    },
-    function (player_info, callback) {
-      console.log(player_info)
-      self.getMatchInfo(region, player_info[name].id, callback)
-    }
+    self.getSummonerInfo(region, name, callback);
+  },
+  function (player_info, callback) {
+    console.log(player_info);
+    self.getMatchInfo(region, player_info[name].id, callback);
+  }
   ], function (err, spectate_data) {
     console.log(spectate_data);
     if (err) return cb(err);
     cb(null, spectate_data);
-    // cb(null, {
-    //   gameId: results.gameId,
-    //   spectateKey : results.observers.encryptionKey
-    // })
-  })
-
-}
+  });
+};
