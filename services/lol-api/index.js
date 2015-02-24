56dcd38292a2d66e8dbb6d7f527723df66069d18
Christian <christian.sherland@dowjones.com>
2015-02-23 21:56:25 -0500

    cleanup

--- services/lol-api/index.js
+++ services/lol-api/index.js
@@ -1,33 +1,34 @@
 var request = require('request'),
-async = require('async'),
-API_KEY = process.env.API_KEY;
+    async = require('async'),
+    API_KEY = process.env.API_KEY;
 
 module.exports = LOL_API = {};
 
 // Template strings don't work :(
 //  var requestUrl = 'https://${region}.api.pvp.net/api/lol/${region}/v1.4/summoner/by-name/${summoner_name}?api_key=${API_KEY}';
 LOL_API.getSummonerInfo = function(region, summoner_name, cb) {
-  var requestUrl = 'https://'+
-  region +'.api.pvp.net/api/lol/'+
-    region +'/v1.4/summoner/by-name/'+
-    summoner_name +'?api_key='+
-    API_KEY;
+  var requestUrl = 'https://' +
+                    region + '.api.pvp.net/api/lol/' +
+                    region + '/v1.4/summoner/by-name/' +
+                    summoner_name + '?api_key=' +
+                    API_KEY;
+
   request.get(requestUrl, function (err, response, body) {
-    if (err) return cb(err);
+    if (err) { return cb(err); }
     cb(null, JSON.parse(body));
   });
 };
 
 // var requestUrl = 'https://${region}.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/${platform_id}/${player_id}?api_key=${API_KEY}';
-
 LOL_API.getMatchInfo = function (region, player_id, cb) {
   region = 'na';
   var platform_id = 'NA1'; // TODO: MAP region to platformID
 
-  var requestUrl = 'https://'+
-  region +'.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/'+
-    platform_id +'/'+ player_id +'?api_key='+
-    API_KEY;
+  var requestUrl = 'https://' +
+                    region + '.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/' +
+                    platform_id + '/' +
+                    player_id + '?api_key=' +
+                    API_KEY;
 
   request.get(requestUrl, function (err, response, body) {
     if (err) return cb(err);
