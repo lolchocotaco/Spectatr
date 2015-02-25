c3cbf5a05ef351b2e3128432c28cb9334d7132f7
Christian <christian.sherland@dowjones.com>
2015-02-24 22:03:57 -0500

    idk

--- services/cache/index.js
+++ services/cache/index.js
@@ -24,13 +24,16 @@ module.exports = cacheSvc = {};
  *
  */
 cacheSvc.getValue = function(namespace, timeout, dataProvider, cb) {
+  console.log(mc);
   mc.get(namespace, function(err, cachedValue) {
-    if (!cachedValue) {
+    console.log(cachedValue);
+    console.log(cachedValue.length);
+    if (!cachedValue.length) {
       dataProvider(function(err, data) {
-        console.log(data);
         mc.set(namespace, data, function(err, value) {
+          console.log(data);
           console.log(value);
-          cb(err, value);
+          cb(err, data);
         }, timeout);
       });
     } else {
