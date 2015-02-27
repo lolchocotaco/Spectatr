2ce6fc209002bc12512d7d6ebff8f38a2c846129
Christian <christian.sherland@dowjones.com>
2015-02-26 22:39:05 -0500

    Sending a test text message

--- services/text-message/index.js
+++ services/text-message/index.js
@@ -7,18 +7,21 @@
 
 // Twilio API creds
 var API_KEY;
-var AUTH_TOKEN = '[AuthToken]';
+var AUTH_TOKEN;
+var PHONE_NUMBER;
 
-// Read the API Key 
+// Read the API Key
 var KEY_FILE = require('./api_key.js');
 if (!API_KEY) {
   API_KEY = KEY_FILE.API_KEY;
+  AUTH_TOKEN = KEY_FILE.AUTH_TOKEN;
+  PHONE_NUMBER = KEY_FILE.PHONE_NUMBER;
 }
 
 // Create text message service
 var twilio = require('twilio')(API_KEY, AUTH_TOKEN);
 
-module.exports = TEXT_MESSAGE = {};
+module.exports = TEXT_MESSAGE = {}; // expose the module
 
 /**
  * sendMessage {function}
@@ -29,5 +32,11 @@ module.exports = TEXT_MESSAGE = {};
  *
  */
 TEXT_MESSAGE.sendMessage = function(message, address, cb) {
-  cb(null)
+  client.messages.create({
+    body: "Sent from twilio API",
+    to: '+16318276915',
+    from: PHONE_NUMBER,
+  }, function(err, message) {
+    console.log(message.sid);
+  });
 };
